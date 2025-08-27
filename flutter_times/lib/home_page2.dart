8b031cd0d38b1e4f8a12df962fd41adfcef5de91
CaparicaLeo <57023740005@unicentro.edu.br>
2025-08-27 09:41:46 -0300

    primeira aplicação

---
+++ flutter_times/lib/home_page2.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage2 extends StatelessWidget {
+  const HomePage2({super.key});
+
+  void decrement() {
+    print('Decrementou');
+  }
+  void increment(){
+    print('Incrementou');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
