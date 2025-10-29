826a9cc20a9bead579dfdfdb42263ee51fd775e5
CaparicaLeo <57023740005@unicentro.edu.br>
2025-10-29 09:30:37 -0300

    apk auth initialized

---
+++ auth_apk/README.md
@@ -0,0 +1,3 @@
+# auth_apk
+
+A new Flutter project.
---
+++ auth_apk/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:auth_apk/view/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: LoginPage(), debugShowCheckedModeBanner: false));
+}
